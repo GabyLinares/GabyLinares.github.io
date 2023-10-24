1f495488707a9bf7746330fdb73c5d253feb8b5f
GabyLinares <gabilina20@gmail.com>
2023-10-23 22:31:44 -0500

    typewriter

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed-text", {
+    strings: ["Desarrolladora", "Ethical Hacker", "Placeholder"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
